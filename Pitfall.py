4e62098ab06ba56d0ee5650be6dad534ec744e0c
Gabriela M. Boriero <gabrielamb2@al.inper.edu.br>
2019-05-21 10:49:54 -0300

    game over

--- Pitfall.py
+++ Pitfall.py
@@ -259,7 +259,34 @@ class LIVES(pygame.sprite.Sprite):
             #self.kill()
         pass
 
+class button():
+    def __init__(self, color, x,y,width,height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
 
+    def draw(self,win,outline=None):
+        #Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x-2,self.y-2,self.width+4,self.height+4),0)
+            
+        pygame.draw.rect(win, self.color, (self.x,self.y,self.width,self.height),0)
+        
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 60)
+            text = font.render(self.text, 1, (0,0,0))
+            win.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def isOver(self, pos):
+        #Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+            
+        return False
 
 
 
@@ -404,6 +431,7 @@ def game_screen(screen):
     # Carrega o fundo do jogo
     background = assets["background"]
     background_rect = background.get_rect()
+    greenButton=button((0,255,0),150,255,250,100, 'continuar')
     
 #    # Carrega os sons do jogo
 #    pygame.mixer.music.load(path.join(snd_dir, 'tgfcoder-FrozenJam-SeamlessLoop.ogg'))
@@ -526,9 +554,10 @@ def game_screen(screen):
                 vida.kill()
             if lives <= 0:
                 player.kill()
-                state = DONE
+#                state = DONE
                 background = assets["game_over"]
                 background_rect = background.get_rect() 
+                
 #                explosao = Explosion(player.rect.center, assets["explosion_anim"])
 #                all_sprites.add(explosao)
 #                state = EXPLODING
